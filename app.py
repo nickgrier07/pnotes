276ee4f46f306b5752181d2a99ff0b48cd67ba82
nickgrier07 <arronngrier0707@gmail.com>
2018-08-31 17:16:10 -0600

    made headers in files, and added initial structures

---
+++ app.py
@@ -0,0 +1,7 @@
+#Project Author: Nick Grier
+#Date:           08/31/18
+#Doc:            This file contains the class definition
+
+class p_notes(object):
+
+    __init__(self,)
---
+++ main.py
@@ -0,0 +1,11 @@
+#Project Author: Nick Grier
+#Date:           08/31/18
+#Doc:            This file executes the app
+import app
+
+def main():
+
+
+
+if __name__='__main__':
+    main()
