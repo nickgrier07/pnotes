f861d2b9af53a616e1e0e333530dea0c299453a8
nickgrier07 <arronngrier0707@gmail.com>
2018-08-31 17:43:39 -0600

    made some function headings

--- app.py
+++ app.py
@@ -2,6 +2,20 @@
 #Date:           08/31/18
 #Doc:            This file contains the class definition
 
+import os, sys
+
 class p_notes(object):
 
-    __init__(self,)
+    def __init__(self,):
+
+
+    def make_new_data(self, header, *notes):
+
+        data = dict()
+
+    def append_old_data(self, *notes):
+
+        data = dict()
+
+    def save_data(self, dict)
+        
